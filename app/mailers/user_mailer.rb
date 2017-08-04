6bc03ed20cf43f7637d69edb266916c565860ba4
Marshall Danel <marshalldanel@gmail.com>
2017-08-04 13:53:45 -0700

    Update mailer to use mailgun

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'no-reply@jungle.com'
+  default from: 'no-reply@'+ ENV['domain']
 
   def order_confirmation(order)
     @order = order
-    mail(to: @order.email, subject: "Here's the Jungle recipt for Order #{ @order.id }!")
+    mail to: @order.email, subject: "Here's the Jungle recipt for Order #{ @order.id }!"
   end
 
 end
