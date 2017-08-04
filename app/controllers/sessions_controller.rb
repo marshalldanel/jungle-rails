568a97e633b1dfc4ee2ede5859798a8d1ef06ee5
Marshall Danel <marshalldanel@gmail.com>
2017-08-03 19:00:21 -0700

    Updated redirect/create new path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
   
+  def new
+  end
+
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to :products [:index]
+      redirect_to '/'
     else
       redirect_to '/login'
     end
