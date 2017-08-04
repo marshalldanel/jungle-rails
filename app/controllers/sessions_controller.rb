e080bd7630e60fb23d0a01a0319402b26a1696d4
Marshall Danel <marshalldanel@gmail.com>
2017-08-03 19:09:27 -0700

    Update redirect route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to :products [:index]
+      redirect_to '/'
     else
       redirect_to '/login'
     end
