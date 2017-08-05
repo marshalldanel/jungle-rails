4f589b701985737afb4394bd6fdabd6505b9042b
Marshall Danel <marshalldanel@gmail.com>
2017-08-05 11:09:12 -0700

    Added ability to view, create reviews for each individual product

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+   def new
+  end
+  
+  def create
+    @product = Product.find(params[:product_id])
+    review = Review.new(review_params)
+
+     if review.save
+      redirect_to :back
+    else
+      redirect_to :back
+    end
+  end
+
+ private
+    def review_params
+      params.require(:review).permit(:product_id, :user_id, :description, :rating)
+    end
+end
