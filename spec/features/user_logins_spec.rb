dd85123e0ce234d57c4e5adb0d57e5bebe452815
Marshall Danel <marshalldanel@gmail.com>
2017-08-08 13:06:53 -0700

    Commit feature spec: User login

---
+++ spec/features/user_logins_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "UserLogins", type: :feature, js: true do
+  
+  before :each do
+    @user = User.create ({
+      first_name: 'Bob',
+      last_name: 'Bob',
+      email: 'bob@bob.com',
+      password: '12345678',
+      password_confirmation: '12345678'
+    })
+  end
+
+  scenario "They see product details by clicking the img/product title" do
+    visit root_path
+    find_link('Login').click
+
+    find 'form' do
+      fill_in id: 'email', with: @user.email
+      fill_in id: 'password', with: @user.password
+
+      click_button 'Submit'
+    end
+    
+    expect(page).to have_content @user.first_name && 'Logout'
+  end
+end
